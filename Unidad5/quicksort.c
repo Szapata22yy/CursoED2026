70779f9bbd24004ede39c066e16c3dd2ddcefedd
Sebastian Zapata <sbxxs1415@gmail.com>
2026-05-26 16:11:38 -0500

    correccion2

--- Unidad5/quicksort.c
+++ Unidad5/quicksort.c
@@ -14,7 +14,7 @@ int partition(int arr[], int low, int high)
     int i = low + 1; 
     for (int j = low + 1; j <= high; j++) 
     { 
-        if (arr[j] < pivot) 
+        if (arr[j] > pivot) 
         { 
             swap(&arr[i], &arr[j]); 
             i++; 
